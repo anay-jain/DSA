29bef839880eb692641727088a5a2455d972de76
anay-jain <anayjain98@gmail.com>
2019-07-29 21:19:16 +0530

    added commnets

--- count_bits.py
+++ count_bits.py
@@ -5,11 +5,13 @@ def count_bits(x):
     count = 0
     while x:
         count += x & 1
+        # it will give us the and of last bit and 1 
+        # then we right shift the last digit and it gets eliminated. 
         x >>= 1
         print x
 
     return count
 
-print count_bits(15)
+print count_bits(45)
 
 
