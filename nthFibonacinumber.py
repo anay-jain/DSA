0ae9db8818a9b9c755251642d5e9cfe910eeacb0
anay-jain <anayjain98@gmail.com>
2019-08-14 22:51:12 +0530

    fiboaacci

--- nthFibonacinumber.py
+++ nthFibonacinumber.py
@@ -1,4 +1,12 @@
 def fib(n):
     memo = [0] * (n+1) # declaring all variables to 0 upto n 
-    
+    memo[0] , memo[1] = 0 , 1
+    for i in range(2 , n+1):
+        memo[i] = memo[i-1] + memo[i-2]
+        print(memo[i])
+    return memo[n]
+
+
+n = int(input())
+print(fib(n))   
 
