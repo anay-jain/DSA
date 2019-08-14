7334c36b315015bec5a34cbd4ba951fcaa8eb4cb
anay-jain <anayjain98@gmail.com>
2019-08-14 23:44:53 +0530

    a

--- nthFibonacinumber.py
+++ nthFibonacinumber.py
@@ -3,10 +3,12 @@ def fib(n):
     memo[0] , memo[1] = 0 , 1
     for i in range(2 , n+1):
         memo[i] = memo[i-1] + memo[i-2]
-        print(memo[i])
+        memo[i]
     return memo[n]
 
-
-n = int(input())
-print(fib(n))   
+t  =  int(input())
+for i in range(t):
+    n = int(input())
+    result = fib(n) % 1000000007
+    print(result)   
 
