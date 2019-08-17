3ef52f51800332c47e87a7d17f19f1c447362f36
anay-jain <anayjain98@gmail.com>
2019-08-17 20:09:35 +0530

    reached a given no

--- geeksforgeeks/reach_a_given_score.py
+++ geeksforgeeks/reach_a_given_score.py
@@ -1,4 +1,15 @@
 for _ in range(int(input())):
     n = int(input())
     memo = [0 for i in range(n+1)]
+    memo[0] = 1
+    for i in range(n+1):
+        if memo[i] == 0:
+            if i>2:
+                memo[i] += memo[i-3]
+            if i>4:
+                memo[i] += memo[i-5]
+            if i>9:
+                memo[i] += memo[i-10]
+    print(memo[n])
+
     
