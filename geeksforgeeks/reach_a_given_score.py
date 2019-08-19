9da9a09a352bd47084754a001b6e5075bf781aea
anay-jain <anayjain98@gmail.com>
2019-08-19 13:31:56 +0530

    reach a given score updated

--- geeksforgeeks/reach_a_given_score.py
+++ geeksforgeeks/reach_a_given_score.py
@@ -2,14 +2,11 @@ for _ in range(int(input())):
     n = int(input())
     memo = [0 for i in range(n+1)]
     memo[0] = 1
-    for i in range(n+1):
-        if memo[i] == 0:
-            if i>2:
-                memo[i] += memo[i-3]
-            if i>4:
-                memo[i] += memo[i-5]
-            if i>9:
-                memo[i] += memo[i-10]
+    coins= [3,5,10]
+    for coin in coins:
+        for i in range(coin ,n+1):
+            memo[i]+=memo[i-coin]
+            
     print(memo[n])
 
     
