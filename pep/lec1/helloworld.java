211c43e256c96ab1f7668e7e5a309e419e766da2
anay-jain <anayjain98@gmail.com>
2019-09-14 10:47:47 +0530

    javac

--- pep/lec1/helloworld.java
+++ pep/lec1/helloworld.java
@@ -1,5 +1,6 @@
 import java.util.*;
-public class solution{
+// classr file  name should be same as your filename
+public class helloworld{ 
     public static void main(String[] args){
         System.out.println("hello world");
         Scanner scanner = new Scanner(System.in);
