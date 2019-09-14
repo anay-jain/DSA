d212e4eb0f5a6883e8eb95442a7d4915d28159f4
anay-jain <anayjain98@gmail.com>
2019-09-14 10:02:24 +0530

    added cpp and python

---
+++ pep/basics/tuts/hello.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class solution {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello there");
+    }
+
+}
