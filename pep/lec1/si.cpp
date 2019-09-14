43e02752bbacf646b467bc781ab841362f8cebef
anay-jain <anayjain98@gmail.com>
2019-09-14 11:18:32 +0530

    simple interest in java

---
+++ pep/lec1/si.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int p = 100 , r=2, t=1;
+    float result = (p*r*t)/100;
+    cout<<"The result is"<<result;
+    return 0;
+}
---
+++ pep/lec1/si.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class si{
+    public static void main(String[] args){
+        // Scanner scanner = new Scanner();
+        // int a = scanner.nextInt();
+        // int b = scanner.nextInt();
+        // int c = scanner.nextInt();
+        int a = 100;
+        int b  = 4;
+        int c = 5;
+        float res = (a*b*c)/100;
+        System.out.println(res);
+
+    }
+}
