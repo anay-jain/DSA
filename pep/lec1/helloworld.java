b3bd1bd129a0f4cb424ac0b4b4590d29f5a8d4e5
anay-jain <anayjain98@gmail.com>
2019-09-14 10:34:22 +0530

    hello in java

---
+++ pep/lec1/helloworld.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class solution{
+    public static void main(String[] args){
+        System.out.println("hello world");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        scanner.close();
+        System.out.println(a);
+        
+    }
+}    
