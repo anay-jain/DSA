13369348566c240ac71c9a37eb4cb02c7e8b3f14
anay-jain <anayjain98@gmail.com>
2019-09-15 09:04:35 +0530

    no of digits

---
+++ pep/lec2/noofdigits.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,count=0;
+    cin>>n;
+    while(n){
+        n = n/10;
+        count++;
+    }
+    cout<<count;
+    return 0;
+}
