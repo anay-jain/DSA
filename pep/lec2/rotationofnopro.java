15af9a7ca7d7a05a58ab1bf203a32cb4b3fe9fa5
anay-jain <anayjain98@gmail.com>
2019-09-15 14:48:40 +0530

    pro and correct solution of rotation of the number

--- pep/lec2/rotationofnopro.java
+++ pep/lec2/rotationofnopro.java
@@ -1,41 +1,43 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class rotationofnopro{
     public static Scanner scn = new Scanner(System.in);
     public static void main(String[] args){
         int a = scn.nextInt();
         int b = scn.nextInt();
-        rotation(a, b);
+        System.out.println(rotation(a,b));
 
-        
 
     }
-    public static int noOfDigits(int a){
-        int count = 0;
+    public static int reverseOfNo(int a ){
+        int count =0;
         while(a!=0){
-            count++;
+            count ++;
             a/=10;
+
         }
         return count;
+        
+    }
+
+    public static int rotation(int a , int k){
+        int digit = reverseOfNo(a);
+        k=k%digit;
+        int mul=1,div=1;
+        k = k<0?k+digit:k;
+        for(int i=1;i<=digit;i++){
+            if(i<=k){
+                mul*=10;
+            }
+            else{
+                div*=10;
+            }
+        }
+        int rem = a%div;
+        a/=div;
+        return (rem*mul) + a;
+
 
     }
-    public static void rotation(int r , int k){
-        int digit=noOfDigits(r);
-        r=r%digit;
-        r = r<0?r+digit:r;
-        int temp = digit-r;
-        int t=0;
-        while (temp!=0) {
-            t = (t*10) + (r%10);
-            temp--;
-            
-        }
-        while(r!=0){
-        t +=((t*10) + r%10);
-        r/=10;
-        }
-        System.out.println(t);
-    }
-    
-    
+
 }
