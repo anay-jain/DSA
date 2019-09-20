2c6f622e16a82ff8da17dc15ac55e04f6d6f2231
anay-jain <anayjain98@gmail.com>
2019-09-20 21:21:47 +0530

    bionomialpattern completed

--- pep/lec3/binomialpattern.java
+++ pep/lec3/binomialpattern.java
@@ -1,20 +1,22 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class binomialpattern{
+
     public static Scanner scn = new Scanner(System.in);
     public static void main(String[] args){
-        int n = scn.nextInt();
-        int nsp=1;
-        for(int i=1;i<=n;i++){
-            for(int csp=1;csp<=nsp;csp++){
-                System.out.print("1");
+        int a = scn.nextInt();
+        bionomial(a);
+    }
+    public static void bionomial(int x){
+        int nno=1;
+        for(int i =0;i<x;i++){
+            int res=1;
+            for(int cno=0;cno<nno;cno++){
+                System.out.print(res + " "); 
+                res = ((i-cno)*res)/(cno+1);   
             }
-            nsp++;
+            nno++;
             System.out.println("");
         }
-        public static void bionomial(int n){
-            
-        }
-
     }
 }
