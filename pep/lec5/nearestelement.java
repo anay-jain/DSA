760dba322f5ed63361234ce890dfc7b46a616fde
anay-jain <anayjain98@gmail.com>
2019-09-22 12:12:57 +0530

    done with lec5

---
+++ pep/lec5/nearestelement.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class nearestelement{
+    public static void main(String[] args){
+
+
+    }
+    
+}
---
+++ pep/lec5/paired.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+bool binarysearch(vector<int>& v,int data , int si,int ei){
+    
+    while(si<=ei){
+        int mid=(si+ei)/2;
+         cout<<v[mid];
+        if(data == v[mid]){
+            return true;
+        }
+        else if(v[mid] > data){
+            ei = mid-1;
+        }
+        else{
+            si= mid+1;
+        }
+    }
+    return false;
+}
+
+void input(vector<int>& v ,int n){
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+}
+int main(){
+    int n,data ;
+    cin>>n;
+    vector<int> v(n,0);
+    input(v,n);
+    cin>>data;
+    cout<<binarysearch(v,data,0,n);
+
+    return 0;
+}
