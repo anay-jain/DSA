8f8c256f6868b4afb901a340c7b7aaa9c554e59e
anay-jain <anayjain98@gmail.com>
2019-09-25 21:15:59 +0530

    add two array

---
+++ pep/lec5/addtwoarray.java
@@ -0,0 +1,12 @@
+import  java.util.Scanner;
+
+public class addtwoarray{
+
+    public static Scanner scn = new Scanner(System.in);
+    public static void main(String[] args){
+
+    }
+    public static int[] addarray(int[] v){
+        
+    }
+}
