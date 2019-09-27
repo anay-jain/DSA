de2e4cbcceeea165b272fa464aa505a27860cfe7
anay-jain <anayjain98@gmail.com>
2019-09-28 04:47:35 +0530

    kcon codechef

---
+++ codechef/questions/KCON.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class KCON{
+    public static Scanner scn = new Scanner(System.in);
+    public static void main(String[] args){
+        
+    }
+}
--- hackerrank/pepdec2017/pattern9.java
+++ hackerrank/pepdec2017/pattern9.java
@@ -4,16 +4,24 @@ public class pattern9{
     public static Scanner scn = new Scanner(System.in);
     public static void main(String args[]){
         int n = scn.nextInt();
-        int nsp=(n/2)+1 , nst=0;
-        for(int i=1;i<n;i++){
+        int nsp=(n/2), nst=0;
+        for(int i=1;i<=n;i++){
             // int nst=(n-i+1);
             for(int csp=1;csp<=nsp;csp++){
                 System.out.print(" ");
             }
             int nstval= nst;
-            for(int cst=1;cst <=(2*nst+1);cst++){
-                System.out.print(nstval);
-                nstval++;
+            int nno=2*nst+1;
+            for(int cst=1;cst <=nno;cst++){
+                if(cst<=(nno/2)+1){
+                    nstval++;
+                }
+                else{
+                    nstval--;
+                }
+                
+                System.out.print(nstval+ " ");
+                
             }
             if(i<=n/2){
                 nsp--;
