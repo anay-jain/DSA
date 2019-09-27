41acfef8a529d9221934c5f9df111a3886f527a0
anay-jain <anayjain98@gmail.com>
2019-09-27 23:32:55 +0530

    pattern9

--- hackerrank/pepdec2017/pattern9.java
+++ hackerrank/pepdec2017/pattern9.java
@@ -4,15 +4,25 @@ public class pattern9{
     public static Scanner scn = new Scanner(System.in);
     public static void main(String args[]){
         int n = scn.nextInt();
-        int nsp=(n/2)+1;
+        int nsp=(n/2)+1 , nst=0;
         for(int i=1;i<n;i++){
-            int nst=(n-i+1);
+            // int nst=(n-i+1);
             for(int csp=1;csp<=nsp;csp++){
-                System.out
+                System.out.print(" ");
             }
+            int nstval= nsp;
             for(int cst=1;cst <=(2*nst+1);cst++){
-
+                System.out.print(nstval);
             }
+            if(i<=n/2){
+                nsp--;
+                nst++;
+            }
+            else{
+                nsp++;
+                nst--;
+            }
+            System.out.println();
         }
 
         
