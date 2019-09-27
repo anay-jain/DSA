df2a9f986c618929408604c57c41fd4c0d0807dd
anay-jain <anayjain98@gmail.com>
2019-09-27 23:18:18 +0530

    created pattern 2

---
+++ hackerrank/pepdec2017/pattern9.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class pattern9{
+    public static Scanner scn = new Scanner(System.in);
+    public static void main(String args[]){
+        int n = scn.nextInt();
+        int nsp=(n/2)+1;
+        for(int i=1;i<n;i++){
+            int nst=(n-i+1);
+            for(int csp=1;csp<=nsp;csp++){
+                System.out
+            }
+            for(int cst=1;cst <=(2*nst+1);cst++){
+
+            }
+        }
+
+        
+    
+    }
+    
+}
