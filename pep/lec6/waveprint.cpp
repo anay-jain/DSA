a5d2215d09cf16918eb7bc342f24493c3e32b8a9
anay-jain <anayjain98@gmail.com>
2019-09-29 10:18:29 +0530

    wave print in cpp

--- pep/lec6/waveprint.cpp
+++ pep/lec6/waveprint.cpp
@@ -2,8 +2,47 @@
 #include<vector>
 
 using namespace std;
+void input(vector<vector<int>> &v){
+    for(int row=0;row<v.size();row++){
+        for(int col=0;col<v.size();col++){
+            
+            cin>>v[row][col];
+        }
+    }
+}
+void display(vector<vector<int>> &v){
+    for(int row=0;row<v.size();row++){
+        for(int col=0;col<v.size();col++){
+            cout<<v[row][col]<<" ";
+        }
+        cout<<endl;
+    }
+}
 
 void waveprint(vector<vector<int>> &v){
-    
+    for(int row=0;row<v.size();row++){
+        if(row%2==0){
+            for(int col=0;col<v.size();col++){
+                cout<<v[row][col]<<" ";
+            }
+        }
+        else{
+            for(int col=v.size()-1;col>=0;col--){
+                cout<<v[row][col]<<" ";
 
+            }
+        }
+        cout<<endl;
+    }
+
+}
+int main(){
+    int n,m;
+    cin>>n>>m;
+    vector<vector<int>>v(n,vector<int>(m,0));
+    input(v);
+    waveprint(v);
+    // display(v);
+
+    return 0;
 }
