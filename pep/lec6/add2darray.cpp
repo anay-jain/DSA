127871194b4bf0db0ef278ce2012701a969e9008
anay-jain <anayjain98@gmail.com>
2019-09-29 09:54:51 +0530

    add array

--- pep/lec6/add2darray.cpp
+++ pep/lec6/add2darray.cpp
@@ -10,11 +10,30 @@ void input(vector<vector<int>> &v){
         }
     }
 }
-void display(vector<vector<int>> &v)
+void display(vector<vector<int>> &v){
+    for(int row=0;row<v.size();row++){
+        for(int col=0;col<v.size();col++){
+            cout<<v[row][col];
+        }
+    }
+}
+void addarray(vector<vector<int>> &v, vector<vector<int>> &v1){
+    for(int row=0;row<v.size();row++){
+        for(int col=0;col<v.size();col++){
+            v[row][col] = v[row][col]+v1[row][col];
+        }
+    }
+
+}
 int main(){
     int n ,m;
     cin>>n>>m;
     vector<vector<int>>v(n,vector<int>(m,0));
+    vector<vector<int>>v1(n ,vector<int>(m,0));
+    input(v);
+    input(v1);
+    addarray(v,v1);
+    display(v);
 
     return 0;
 }
--- pep/lec6/waveprint.cpp
+++ pep/lec6/waveprint.cpp
@@ -1,3 +1,9 @@
-#include <iostream>
-#include <vector>
+#include<iostream>
+#include<vector>
 
+using namespace std;
+
+void waveprint(vector<vector<int>> &v){
+    
+
+}
