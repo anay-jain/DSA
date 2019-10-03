9d044232d4a2f6f01042b49acf753acb694cef81
anay-jain <anayjain98@gmail.com>
2019-10-03 18:57:26 +0530

    bubble selection and modified bubble

---
+++ pep/lec7/bubble.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+void input(vector<int> &v){
+    int n =v.size();
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+}
+
+void display(vector<int> &v){
+    int n = v.size();
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<"  ";
+    }
+}
+void swap(vector<int> &v,int i,int j){
+    int temp;
+    temp=v[i];
+    v[i]=v[j];
+    v[j]=temp;
+    
+}
+
+void bubble(vector<int> &v){
+    int n=v.size();
+    for(int i =0;i<n;i++){
+        for(int j=1;j<n-i;j++){
+            if(v[j-1]>v[j]){
+                swap(v,j-1,j);
+            }
+        }
+    }
+}
+
+void selection(vector<int> &v){
+    int n = v.size();
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(v[i]>v[j]){
+                swap(v,i,j);
+            }
+        }
+    }
+}    
+void modifiedbubble(vector<int> &v){
+    int n = v.size();
+    for(int i=0;i<n;i++){
+        int flag=false;
+        for(int j=1;j<n-i;j++){
+            if(v[j-1]>v[j]){
+                swap(v,j-1,j);
+                flag=true;
+            }
+           
+        }
+         if(flag==false){
+                cout<<"flag is called at  "<<i<<endl;
+                break;
+            }
+    }
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> v(n,0);
+    input(v);
+    // bubble(v);
+    // selection(v);
+    modifiedbubble(v);
+    display(v);
+
+
+    return 0;
+}
