3f0f644dbd7ef4c6643b985e85173874bf750310
anay-jain <anayjain98@gmail.com>
2019-10-05 11:48:39 +0530

    better solution of power of 2

--- pep/lec8/apowerb.java
+++ pep/lec8/apowerb.java
@@ -3,14 +3,27 @@ import java.util.Scanner;
 public class apowerb{
     public static Scanner scn = new Scanner(System.in);
     public static void main(String[] args){
-        System.out.println(power(2,5));
+        System.out.println(betterpower(2,100));
 
     }
-    public static int power(int a , int b){
+    public static int power11(int a , int b){
         if(b==1){
             return a;
         }
-        int res = power(a,b-1);
+        int res = power11(a,b-1);
         return res*a;
+    }
+    public static int betterpower(int a , int b){
+        if(b==0){
+            return 1;
+        }
+        
+        int res = betterpower(a, b/2);
+        int final1 = res*res;
+        if(b%2==1){
+            final1*=a;
+        }
+        return final1;
+        
     }
 }
