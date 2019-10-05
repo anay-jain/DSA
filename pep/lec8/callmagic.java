b8f061aecaa2c88a4653b797df20139d7a454d9c
anay-jain <anayjain98@gmail.com>
2019-10-05 21:12:10 +0530

    call magic in java

---
+++ pep/lec8/callmagic.java
@@ -0,0 +1,24 @@
+public class callmagic{
+
+    public static void main(String[] args) {
+        System.out.println(calls(5));
+        
+    }
+
+    public static int calls(int n ){
+        int count =0;
+        if(n<=1){
+            System.out.println("Base : "+ n  + "Count : " + count);
+            return n;
+        }
+       
+        System.out.println("Pre : " + n + "Count : " + count);
+        count+=calls(n-1);
+
+        System.out.println("In : "+ n + "Count : " + count);
+
+        count+=calls(n-2);
+        System.out.println("Post : "+ n + "Count : " + count);
+        return count+3;
+    }
+}
