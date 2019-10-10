a66b2806025e3b6658fa2326ab064022a29baabf
anay-jain <anayjain98@gmail.com>
2019-10-10 20:10:23 +0530

    return type

--- pep/lec10/returntype.java
+++ pep/lec10/returntype.java
@@ -3,10 +3,34 @@ import java.util.ArrayList;
 public class returntype{
 
     public static void main(String[] args){
+         System.out.println(subseq("ABC")); 
+
 
     }
 
-    public static ArrayList subseq(str){
+    public static ArrayList <String> subseq( String str){
+        if(str.length()==0){
+            ArrayList base = new ArrayList<>();
+            base.add("");
+            return base;
+        }
+
+        char ch = str.charAt(0);
+        String ros = str.substring(1);
+
+        ArrayList <String> resArray = subseq(ros);
+        // now we will creare  a new array that will haave all value of res array;
+        ArrayList <String> myAns = new ArrayList<>(); // we will copy all elements to another array
+        myAns.addAll(resArray); // if element not taken
+
+        for(String s : resArray){
+            myAns.add(ch+s); // if element taken
+        }
+        return myAns;
+
+
+
+
         // if(str.size()==0){
         //     return 
         // }
@@ -14,6 +38,13 @@ public class returntype{
         //     print
         // return str1;
         
+    }
+    public static String removeHi(String ques){
+        if(hi){
+            String res  = ques.substring(beginIndex)
+        }
+
+
     }
     
 }
