c7ce2720d433b2b1621e283081c7c60522925683
anay-jain <anayjain98@gmail.com>
2019-10-13 11:06:40 +0530

    completed return type

---
+++ pep/lec11/mazepath.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+public class mazepath{
+    public static void main(String[] args){
+
+    }
+
+        // basecase
+        if(sr==er &&  ec==es){
+            ArrayList <String> finalans = new ArrayList<>();
+            finalans.append(recAns);
+            return finalans;
+        }
+
+       
+        
+        ArrayList <String> finalans = mazepath1(sr+1, se+1, ec, er);
+        if(sr<er){
+            String recAns = "h" + mazepath1(sr+1, se, ec, er);
+        }
+        if(sc<er){
+            String recAns = "v" + mazepath1(sr, se+1, ec, er);
+        }
+
+        return finalans;
+
+    }
+
+    
+
+}
