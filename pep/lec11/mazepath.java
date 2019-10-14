4d53a9f4abda7c3918902ed100e2691bfafbcdc3
anay-jain <anayjain98@gmail.com>
2019-10-14 12:52:06 +0530

    mazepath

--- pep/lec11/mazepath.java
+++ pep/lec11/mazepath.java
@@ -1,11 +1,12 @@
 import java.util.ArrayList;
 public class mazepath{
     public static void main(String[] args){
-        ArrayList <String> ans = mazepath1(0,0,2,2);
+        ArrayList <String> ans = mazepathmulti(0,0,2,2);
         for(String s: ans){
             System.out.print(s + " ");
         }
-        
+        // System.out.println(mazepathhei(0, 0, 2,2));
+            
     }
 
       public static ArrayList <String> mazepath1(int sr,int sc,int er,int ec){
@@ -38,6 +39,59 @@ public class mazepath{
         return ans;
       }
 
+
+      public static int mazepathhei(int sr,int sc,int er,int ec){
+          int maxhei = 0;
+
+        if(sr==er && sc==ec){
+            return 0;
+        }
+
+        if(sr<er){
+        maxhei =  Math.max(maxhei,mazepathhei(sr+1, sc, er, ec));
+       
+    }
+        if(sc<ec){
+        maxhei = Math.max(maxhei , mazepathhei(sr,sc+1,er,ec));
+      
+    }
+        if(sc<ec && sr<er){
+        maxhei = Math.max(maxhei , mazepathhei(sr+1,sc+1,er,ec));
+        
+        }
     
+        return maxhei+1;
+      }
+
+      public static ArrayList <String> mazepathmulti(int sr,int sc,int er,int ec){
+
+        if(sr==er && sc==ec){
+            ArrayList <String> base = new ArrayList<>();
+            base.add("");
+            return base;
+        }
+        ArrayList <String> ans = new ArrayList<>();
+        for(int i=1;sr+i<=er;i++){
+        ArrayList <String> horizontal = mazepathmulti(sr+1, sc, er, ec);
+        for(String s: horizontal){
+            ans.add("h"+ i + s);
+        }
+    }
+        for(int i=1;sc+i<=ec;i++){
+        ArrayList <String> vertical = mazepathmulti(sr,sc+1,er,ec);
+        for(String s: vertical){
+            ans.add("v" + i+s);
+        }
+    }
+        for(int i=1;i<= sc+i && i<=sr+i ;i++){
+            ArrayList <String> diagonal = mazepathmulti(sr+1,sc+1,er,ec);
+            for(String s: diagonal){
+                ans.add("d"+i+s);
+            }  
+        }
+    
+        return ans;
+      }
+
 
 }
