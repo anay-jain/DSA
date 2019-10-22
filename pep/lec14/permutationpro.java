92b653ebde76be991b2187d42fc08098133ce386
anay-jain <anayjain98@gmail.com>
2019-10-22 14:23:57 +0530

    recurs

--- pep/lec14/permutationpro.java
+++ pep/lec14/permutationpro.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
 public class permutationpro{
     public static void main(String[] args){
-        System.out.println(permuation("a"));
+        // System.out.println(encoding("01561"));
+        String[] keys={".","abc","def","ghi","jkl","mno","pqrs","tu","vwx","yz"};
+         System.out.println(keypad_01("245",keys));
     }
     public static ArrayList<String> permuation(String ques){
         // base case
@@ -35,17 +37,84 @@ public class permutationpro{
         
     }
 
-    public static ArrayList<String> encoding(String ques){
-        if(ques.length()==0){
-            ArrayList <String> base = new ArrayList<>();
-            base.add("");
-            return base;
-        }
-        ch = ques.charAt(0);
-        ques=ques.substring(1);
+   public static ArrayList<String> encoding(String ques){
+       if(ques.length()==0){
+           ArrayList<String> base = new ArrayList<>();
+           base.add("");
+           return base;
+       }
+       char ch = ques.charAt(0);
+    //    ques= ques.substring(1);
+       ArrayList<String> ans = new ArrayList<>();
+       if(ch=='0'){
+           return encoding(ques.substring(1));
 
-        if(ch=='0'){
-            
+       }
+       else{
+           ArrayList <String> recAns = encoding(ques.substring(1));
+           // converting int to mapped char and adding it in ans
+           char ch1 = (char)('a' + ch-'1');
+           for(String s:recAns){
+               ans.add(ch1+s);
+               
+           }
+       }
+       if(ques.length()>1){
+           char ch1 = ques.charAt(1);
+           int num = (ch1-'0')*10+(ch-'1');
+           // another methif
+        //    int num = Integer.parseInt(ques.substring(1));
+        if(num<27){
+           ArrayList<String> recAns = encoding(ques.substring(2));
+           for(String s: recAns){
+               char twodigit = (char)('a' + num-1);
+               ans.add(twodigit+s);
+           }
         }
+
+        
+       }
+       return ans;
+   }
+   public static ArrayList keypad_01(String ques , String[] keypad){
+       if(ques.length()==0){
+           ArrayList <String> base = new ArrayList<>();
+           base.add("");
+           return base;
+       }
+       ArrayList ans = new ArrayList<>();   
+       char ch = ques.charAt(0);
+    //    ques = ques.substring(1);
+       int idx= ch- '0';
+       String key = keypad[idx];
+       for(int i=0;i<key.length();i++){
+           ArrayList<String> recAns = keypad_01(ques.substring(i), keypad);
+           for(String s: recAns){
+               ans.add(key.charAt(i)+s);
+           }
+       }
+       return ans;
+   }
+   public static ArrayList<String> keyPad_01(String ques,String[] keys){
+if(ques.length()==0){
+    ArrayList<String> base=new ArrayList<>();
+    base.add("");
+    return base;
+}
+
+    
+        char ch=ques.charAt(0);
+        String roq=ques.substring(1);
+        int idx=ch-'0';
+        String word=keys[idx];
+
+        ArrayList<String> myAns=new ArrayList<>();
+        ArrayList<String> recAns=keyPad_01(roq,keys);
+        for(String s: recAns){
+            for(int i=0;i<word.length();i++)
+               myAns.add(word.charAt(i)+s);
+        }
+         return myAns;
     }
+ 
 }
---
+++ pep/rough/ascii.java
@@ -0,0 +1,7 @@
+public class ascii{
+    public static void main(String[] args){
+        char ch ='2';
+        char t = (char)('a'+'2'-'1' );
+        System.out.println(t);
+    }
+}
