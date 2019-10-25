43906384760e21eda31cfd5cc217b8fa596965c4
anay-jain <anayjain98@gmail.com>
2019-10-25 12:21:13 +0530

    paired

--- pep/rough/binarysearch.cpp
+++ pep/rough/binarysearch.cpp
@@ -2,7 +2,7 @@
 #include<vector>
 using namespace std;
 int bs(vector<int> &arr, int data){
-int si=0, ei=arr.size();
+int si=0, ei=arr.size()-1;
 // int mid = ei%2==0?ei/2:ei+1/2;
 
 while(si<=ei){
@@ -44,10 +44,44 @@ int lowerbound(vector<int> &v,int data){
 }
 
 int nearestelement(vector<int> &v,int data){
-    
+    int si=0,ei=v.size();
+    while(si<=ei){
+        int mid= (si+ei)/2;
+        if(v[mid]==data){
+            break;
+        }
+        else if(v[mid]>data){
+            ei=mid-1;
+        }
+        else{
+            si=mid+1;
+        }
+    }
+    if(data-v[si]>v[ei]-data){
+        return ei;
+    }
+    else{return ei;}
+}
+int paired(vector<int> &v){
+    int si=0,ei=v.size()-1;
+    while(si<=ei){
+        int mid = (si+ei)/2;
+        if(mid+1<=ei && v[mid]==v[mid+1]){
+            ei=mid-1;
+        }
+        else if(mid-1>=si && v[mid-1]==v[mid]){
+            si=mid+1;
+        }
+        else{
+            return v[mid];
+        }
+
+
+    }
 }
 int main(){
-    vector<int> arr = {1,2,3,3,3,3,3,3,4,5,6};
-cout<<lowerbound(arr , 3);
+    vector<int> arr = {1,2,2,3,3,4,4};
+cout<<paired(arr);
+
 return 0;
 }
