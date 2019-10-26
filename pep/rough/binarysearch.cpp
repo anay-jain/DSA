5e6d54785a774eefe630dc19eddd5a1873261007
anay-jain <anayjain98@gmail.com>
2019-10-26 13:23:14 +0530

    bs

--- pep/rough/binarysearch.cpp
+++ pep/rough/binarysearch.cpp
@@ -79,9 +79,79 @@ int paired(vector<int> &v){
 
     }
 }
+void input(vector <int> &v){
+    for(int i =0;i<v.size();i++){
+        cin>>v[i];
+    }
+}
+int pivotbs(vector<int> &v){
+    int si=0,ei=v.size()-1;
+    while(si<=ei){
+        int mid=(si+ei)/2;
+         if(mid-1>=si && v[mid-1]>v[mid]){
+             return mid;
+    }
+    else if(v[mid]<v[si]){
+        ei=mid;
+    }
+    else if(v[mid]>v[si]){
+        si=mid;
+    }
+    else{
+        break;
+    }
+
+    }
+    return -1;
+   
+}
+int searchinpivot(vector <int> &v, int data){
+    
+    int si =0, ei=v.size()-1;
+    int mid = pivotbs(v);
+    cout<<mid;
+    if(mid!=-1){
+        if(data<v[ei]){
+            si=mid;
+        }
+        else{
+            ei=mid-1;
+            cout<<ei;
+
+        }
+    }
+    
+
+    
+    while(si<=ei){
+         mid=(si+ei)/2;
+        if(v[mid]==data){
+            return mid;
+        }
+        else if(v[mid]>data){
+            ei=mid-1;
+        }
+        else{
+            si=mid+1;
+        }
+       
+    }
+    return -1;
+}
 int main(){
-    vector<int> arr = {1,2,2,3,3,4,4};
-cout<<paired(arr);
+    int n,t,data;
+    cin>>t;
+    while(t--){
+        cin>>n;
+    vector<int> v(n,0);
+    input(v);
+    cin>>data;
+    cout<<searchinpivot(v,data);
+    }
+//    vector<int> v = {982 ,983 ,986 ,988 ,990 ,991 ,992 ,998 ,1000, 1, 3, 9 ,11 };
+//    cout<<pivotbs(v);
+
+
 
 return 0;
 }
