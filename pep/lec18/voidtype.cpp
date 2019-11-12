bbee521b59cd6c1092b5454320c17be6b29a1735
anay-jain <anayjain98@gmail.com>
2019-11-13 03:44:02 +0530

    nqueen solve

---
+++ pep/lec18/voidtype.cpp
@@ -0,0 +1,197 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<climits>
+
+using namespace std;
+void subseq(string ques , string ans){
+    if(ques.length()==0){
+        cout<<ans<<" ";
+        return ;
+    }
+    char ch = ques[0];
+    ques = ques.substr(1);
+    subseq(ques,ans+ch);
+    subseq(ques,ans);
+}
+void removeHi(string ques , string ans){
+    
+    if(ques.length()==0){
+        cout<<ans;
+        return ;
+    }
+    
+    if(ques.length()>1 && ques[0]=='h' && ques[1]=='i'){
+        removeHi(ques.substr(2),ans);
+    }
+    else{
+        removeHi(ques.substr(1),ans+ques[0]);
+    }
+} 
+void removeduplicates(string ques , string ans){
+    if(ques.length()==0){
+        cout<<ans;
+        return ;
+    }
+    // if(ques.length()==1){
+    //     ans = ans+ques[0];
+    // }
+    if( ques[0]==ans[ans.length()-1]){
+        removeduplicates(ques.substr(1),ans);
+    }
+    else{
+        removeduplicates(ques.substr(1), ans+ques[0]);
+    }
+}
+void compression(string ques, string ans, int count){
+    if(ques.length()==0){
+        cout<<ans;
+        return ;
+    }
+    if(ques[0]==ans[ans.length()-1]){
+        compression(ques.substr(1),ans,count+1);
+    }
+    else{
+        
+        compression(ques.substr(1),ans+ques[0]+to_string(count), 0);
+    }
+
+}
+
+void mazepath(int sr , int sc , int er , int ec , string ans){
+    if(sr==er && sc==ec){
+        cout<<ans<<" ";
+        return ;
+    }
+    if(sr<er){
+        mazepath(sr+1,sc, er , ec, ans+"h");
+    }
+    if(sc<er){
+        mazepath(sr,sc+1,er,ec,ans+"v");
+    }
+}
+void mazepath_multi(int sr , int sc , int er ,int ec,string ans){
+    if(sr==er && sc==ec){
+        cout<<ans<<" ";
+        return;
+    }
+    for(int i=1;i+sr<=er;i++){
+        mazepath_multi(sr+i,sc,er,ec,ans+"h");
+
+    }
+    for(int i=1;i+sc<=ec;i++){
+        mazepath_multi(sr,sc+i,er,ec,ans+"v");
+    }
+}
+int board_path(int start,int n ){
+    
+    if(start==n){
+        return 1;
+       
+    }
+    if(start>n){
+        return 0;
+    }
+    int c=0;
+for(int i=1;i<=6 && i+start<=n;i++){
+     c +=  board_path(i+start ,n );
+
+}
+    return c;
+}
+void permuation_withrep(string s,string ans){
+    
+}
+void mazepath_question(){
+//  mazepath_multi(0,0,2,2,"");
+// cout<<board_path(0,10);
+
+}
+void basicques(){
+    // subseq("abc","");
+    // removeHi("hishshi" ,"");
+    // removeduplicates("aaaaabbbcc" , "");
+    // compression("aabbccc","", 0);
+    // mazepath_question();
+    //
+}
+int nqueen_combi_01(int boxes , int tnq , int qloc , int qpsf,string ans ){
+if(tnq==qpsf || qloc==boxes ){
+    if(tnq==qpsf){
+        cout<<ans<<endl;
+        return 1;
+    }
+    return 0;
+}
+int count=0;
+for(int i=qloc+1 ;i<boxes;i++){
+    count+=nqueen_combi_01(boxes,tnq,i,qpsf+1,ans+"B"+to_string(i)+"Q"+to_string(qpsf)+" ");
+}
+return count;
+}
+int nqueen_permu_01(int boxes,int tnq , vector<bool> &loc , int qpsf ,string ans){
+    if(tnq==qpsf ){
+    if(tnq==qpsf){
+        cout<<ans<<endl;
+        return 1;
+    }
+   
+    }
+    int count=0;
+    for(int i=0;i<loc.size();i++){
+        if(!loc[i]){
+            loc[i]=true;
+            count+=nqueen_permu_01(boxes,tnq,loc,qpsf+1,ans+"B"+to_string(i)+"Q"+to_string(qpsf)+" ");
+            loc[i]=false;
+        }
+         }
+         return count;
+}
+int nqueen_combi02_target(int boxes , int tnq , int qloc , int qpsf , string ans){
+   if(tnq==qpsf || qloc==boxes ){
+    if(tnq==qpsf){
+        cout<<ans<<endl;
+        return 1;
+    }
+    return 0;
+}
+    int count=0; 
+    count+=nqueen_combi02_target(boxes , tnq , qloc+1,qpsf+1,ans+ "B" + to_string(qloc)+"Q"+to_string(qpsf)+" ");
+    count+=nqueen_combi02_target(boxes , tnq , qloc+1,qpsf,ans);
+    
+    
+    return count;
+}
+int nqueen_permu02_target(int boxes , int tnq, int qloc , int qpsf , vector<bool> &loc , string ans){
+    if(tnq==qpsf || qloc==boxes ){
+    if(tnq==qpsf){
+        cout<<ans<<endl;
+        return 1;
+    }
+    return 0;
+}
+    int count=0;
+    if(!loc[qloc]){
+        loc[qloc]=true;
+        count+=nqueen_permu02_target(boxes, tnq ,qloc+1 ,qpsf+1,loc , ans+"b"+to_string(qloc)+"Q"+to_string(qpsf)+" ");
+        loc[qloc]=false;
+    }
+    count+=nqueen_permu02_target(boxes , tnq , qloc +1 , qpsf , loc , ans);
+    return count;
+}
+void nqueen(){
+    // cout<<nqueen_combi_01(7,3,-1,0,"");
+    vector<bool> loc(8,false);
+    // cout<<nqueen_permu_01(7,3,loc,0,"");
+    // cout<<nqueen_combi02_target(7,3,0,0,"");
+    cout<<nqueen_permu02_target(7,3,0,0,loc,"");
+}
+void solve(){
+    // basicques();
+    nqueen();
+}
+
+int main(){
+    solve();
+    return 0;
+}
