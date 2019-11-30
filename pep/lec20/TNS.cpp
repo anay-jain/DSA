c59c258228637b758b29e4a2c8849969be67fb0c
anay-jain <anayjain98@gmail.com>
2019-11-30 23:47:28 +0530

    completed TNS

--- pep/lec20/TNS.cpp
+++ pep/lec20/TNS.cpp
@@ -6,6 +6,7 @@ void swap(vector<int> &arr , int pi , int si){
     arr[pi]=arr[si];
     arr[si]=temp;
 }
+//==== sort 0 1 and 2 =============================================================================
 void sort0and1(vector<int> &arr ){
     int p1=0,p2=0;
     while(p1<arr.size()){
@@ -16,13 +17,29 @@ void sort0and1(vector<int> &arr ){
         p1++;
     }
 }
+void sort01and2(vector<int> &arr){
+    int itr =0,p0=0,p2=arr.size()-1;
+    while(itr<=p2){
+        if(arr[itr]==0){
+            swap(arr,itr,p0);
+            p0++;
+        }
+        if(arr[itr]==2){
+            swap(arr, itr, p2);
+            p2--;
+            continue;
+        }
+        itr++;
+    }
+}
+//===================================================================================================
 // void sort012(vector<int> &arr){
 //     int p0=0,p1=0,p2=arr.size();
     
 // }
-void pivot(vector<int> &arr, int pivot){
-    int itr  =0,p1=0;
-    while(itr<arr.size()){
+void pivot(vector<int> &arr, int pivot , int li , int ri){
+    int itr  =li,p1=li;
+    while(itr<=ri){
         if(arr[itr]<pivot){ // left condition..
             swap(arr,p1,itr);
             p1++;
@@ -31,6 +48,7 @@ void pivot(vector<int> &arr, int pivot){
     }
 }
 
+//===========MERGE SORT======================================================================
 vector<int> mergesortedarray( vector<int> &left , vector<int> &right){
     int n = left.size()+ right.size();
     vector<int> arr(n,0);
@@ -78,6 +96,7 @@ vector<int> newarr = mergesortedarray( left , right);
 
 return newarr;
 }
+//=========================================================================================================
 void display(vector<int> &arr){
     for(int i=0;i<arr.size();i++){
         cout<<arr[i]<<" ";
@@ -87,10 +106,11 @@ int main(){
     vector<int> oldarr={3,4,45,32,40,78,65};
     // vector<int> arr=mergesort(oldarr, 0,oldarr.size());
     vector<int> zeroone={0,1,1,1,1,0,0,1};
-    sort0and1(zeroone);
+    // sort0and1(zeroone);
 
-    
-    display(zeroone);
+    vector<int> zeroonetwo={0,2,2,1,1,2,0,0,1,1,1,2,2,0,0,1};
+    sort01and2(zeroonetwo);
+    display(zeroonetwo);
 
 
     return 0;
