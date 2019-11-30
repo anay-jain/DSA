55453a3bea5ffe8369968629518d0291b2e5ff1a
anay-jain <anayjain98@gmail.com>
2019-12-01 00:40:43 +0530

    started revision of return tyoe

---
+++ pep/rough/returntype.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class returntype{
+    public static void main(String args[]){
+        System.out.println(subseq("abc"));
+
+    }
+
+    public static ArrayList<String> subseq(String ques){
+        if(ques.length()==0){
+            // return an empty arraylist
+            ArrayList<String> base = new ArrayList<>();
+            base.add(""); // adding an empty string to array list
+            return base;
+        }
+        char ch = ques.charAt(0);
+        String ros = ques.substring(1);
+        ArrayList<String> finalans = new ArrayList<>();
+        ArrayList<String> reference = subseq(ros);
+        for(String s: reference){
+            finalans.add(ch+s);
+        }
+        finalans.addAll(reference);
+
+        return finalans;
+
+    }
+}
