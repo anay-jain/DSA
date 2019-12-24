bf968d3a76ca87a5acffcd8f890b7b185981ceac
anay-jain <anayjain98@gmail.com>
2019-12-24 15:12:23 +0530

    update

--- pep/lec21/graphcpp.cpp
+++ pep/lec21/graphcpp.cpp
@@ -1,8 +1,20 @@
 #include<iostream>
+#include<vector>
 using namespace std;
-class edge()
+class edge{
+    public:
+    int v;
+    int wt;
+    edge(int v , int wt){
+        this->v=v;
+        this->wt=wt;
+    }
+    edge(){
+    }
+};
+
+
 int main(){
-    
 
     return 0;
 }
