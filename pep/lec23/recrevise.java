4c68babf3f9ba0b60999ed6936d24f2d0b4153cb
anay-jain <anayjain98@gmail.com>
2020-01-03 22:54:12 +0530

    nqueen revuse

--- pep/lec23/recrevise.java
+++ pep/lec23/recrevise.java
@@ -79,13 +79,113 @@ public class recrevise{
         return count;
 
     }
+    // lqpl -> last queen placed location
+    public static int queencombi2d(int lqpl, int tnq , int qpsf , int er, String ans ){
+        if(qpsf == tnq || lqpl == (er*er)){
+            if(qpsf==tnq){
+                System.out.println(ans);
+                return 1;
+            }
+            return 0;
+           
+        }        
+        int count=0;
+        int x = lqpl /er;
+        int y = lqpl %er;
+        count+=queencombi2d(lqpl+1, tnq, qpsf+1, er, ans+"b("+x+","+y+")");
+        count+=queencombi2d(lqpl+1, tnq, qpsf, er, ans);
+        return count;
+    }
+    public static int queenpermu2d(int lqpl, int tnq , int qpsf , int er, boolean[] vis , String ans ){
+        if(qpsf == tnq || lqpl == (er*er)){
+            if(qpsf==tnq){
+                System.out.println(ans);
+                return 1;
+            }
+            return 0;
+           
+        }
+        int count=0;
+        int x = lqpl /er;
+        int y = lqpl %er;
+        count+=queencombi2d(lqpl+1, tnq, qpsf+1, er, ans+"b("+x+","+y+") ");
+        count+=queencombi2d(lqpl+1, tnq, qpsf, er, ans);
+        return count;
+    }
+    static int[][] dir = {{-1,0},{-1,-1},{0,-1},{-1,1},{1,0},{1,1},{0,1},{1,-1}};
+    public static boolean issafe(boolean[][] box , int X , int Y){
+        for(int d=0;d<dir.length;d++){
+            for(int rad=1;rad<=Math.max(box.length,box[0].length);rad++){
+                int x = X+ rad*dir[d][0];
+                int y = Y + rad*dir[d][1];
+                if(x>=0 && y>=0 && x<box.length && y <box[0].length){
+                    if(box[x][y]==true) return false;
+                }
+                else{
+                    break;
+                    
+                }
+
+            }
+        }
+        
+        return true;
+    }
+    public static int nqueencombi(boolean[][] box , int idx, int qnf , boolean[] vis,String ans){
+        if(qnf==0 || idx==(box.length*box[0].length)-1){
+            if(qnf==0){
+                System.out.println(ans);
+                return 1;
+            }
+            return 0;
+        }
+        int count=0;
+        for(int i=idx;i<box.length*box[0].length;i++){
+            int x = i/box.length;
+            int y = i%box[0].length;
+            if(issafe(box, x, y)){
+                box[x][y]=true;
+                // System.out.println("fe");
+                count+=nqueencombi(box, i+1, qnf-1, vis, ans+"b("+x+","+y+") ");
+                box[x][y]=false;
+            }
+        }
+        return count;
+    }
+    public static int nqueenpermu(boolean[][] box , int idx, int qnf , boolean[] vis,String ans){
+        if(qnf==0 || idx==(box.length*box[0].length)-1){
+            if(qnf==0){
+                System.out.println(ans);
+                return 1;
+            }
+            return 0;
+        }
+        int count=0;
+        for(int i=0;i<box.length*box[0].length;i++){
+            int x = i/box.length;
+            int y = i%box[0].length;
+            if(!box[x][y] && issafe(box, x, y)){
+                box[x][y]=true;
+                // System.out.println("fe");
+                count+=nqueenpermu(box, 0, qnf-1, vis, ans+"b("+x+","+y+") ");
+                box[x][y]=false;
+            }
+        }
+        return count;
+    }
     public static void main(String[] args){
         // boolean[][] isvisited = new boolean[5][5];
         // System.out.println(mazepath(0, 0, 4, 4 ,0, isvisited));
         // System.out.println(max);
             // System.out.println(subseq("abc"));
-            int coins[]= {2,3,5,7};
-                coins_target(coins, 0, 0, 10, "");
+            // int coins[]= {2,3,5,7};
+            //     coins_target(coins, 0, 0, 10, "");
             // System.out.println(coin_Change_uniqPer(10, coins, "", new boolean[coins.length]));
-    }
+            boolean[][] box = new boolean[4][4];
+            System.out.println(nqueenpermu(box, 0, 4, new boolean[box.length],  ""));
+        
+            // queencombi2d(0, 3, 0,3, "");
+    
+    
+        }
 }
