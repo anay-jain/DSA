51576468ae3687cbd89e6292cb392779c8d0dcba
anay-jain <anayjain98@gmail.com>
2020-01-19 23:06:09 +0530

    update

---
+++ pep/lecStack/myQueue.java
@@ -0,0 +1,16 @@
+public class myQueue{
+    int st=0;
+    int end =0;
+    int size=0;
+    int[] que;
+    myQueue(){
+        int[] que = new int[10];
+
+    }
+    myQueue(int size){
+        int[] que = new int[size];
+    }
+    public void push(int data){
+               
+    }
+}
---
+++ pep/lecStack/myStack.java
@@ -0,0 +1,11 @@
+class myStack{
+    private int st[];
+    int size;
+    myStack(){
+        int[] st = new int[10];
+    }
+    myStack(int size){
+        int[] st = new int[size];
+    }
+    
+}
