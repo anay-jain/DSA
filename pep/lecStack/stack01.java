028cd34875b185695e865e918ad783b1037a3a8a
anay-jain <anayjain98@gmail.com>
2020-01-24 23:17:45 +0530

    btree

--- pep/lecStack/stack01.java
+++ pep/lecStack/stack01.java
@@ -4,10 +4,8 @@ public class stack01{
     static ArrayList<Integer> num = new ArrayList<>();
     static ArrayList<Character> ch = new ArrayList<>();
     public static int priorityseq(char ch ){
-        if(ch=='('){
-            return 0;
-        }
-        else if(ch=='+' || ch =='-' ){
+        
+        if(ch=='+' || ch =='-' ){
             return 1;
         }
         else if(ch=='*' || ch=='/'){
@@ -19,10 +17,24 @@ public class stack01{
         else{
             return 4;
         }
-    }
-    public static String infixToPostfix(String str){
-        
 
+    }
+    public static boolean isch(char ch ){
+        if(ch == '+' || ch=='-' || ch =='/' || ch =='%'){
+            return true;
+        }
+        return false;
+    }
+    public static int calculate(int val1, int val2 , char ch){
+        if(ch =='+')return val2+val1;
+        else if(ch == '-')return val2-val1;
+        else if(ch == '*') return val2*val1;
+        else if(ch=='/') return val2/val1;
+        else return -1;
+    }
+    public static void justnextgreaterele(int[] arr){
+            
+        }
     }
     // Stack st = new Stack()
     public static void main(String[] args){
--- pep/lecTree01/BTree.java
+++ pep/lecTree01/BTree.java
@@ -28,7 +28,8 @@ public class BTree{
         // System.out.println(rootToNodePath02(root, 70, ans));
         // System.out.println(ans);
         // System.out.println(LCA(root, 70, 90));
-        kfar01(root, 2, 60);
+        // kfar01(root, 2, 60);
+        kfar02(root, 2, 60);
     }
     static int idx=0;
     //---- construction -------------------------------
@@ -131,16 +132,18 @@ public class BTree{
         }
         return res;
     }
+    //------- Diameter two methods ------------------------------------------------------
+    
     // -----LCA    ----------------------------------------------------------------------
     public static int LCA(Node node , int data1 , int data2){
-        ArrayList<Integer> node1 = shortestPathToNode(node, data1);
-        ArrayList<Integer> node2 = shortestPathToNode(node, data2);
+        ArrayList<Node> node1 = shortestPathToNode(node, data1);
+        ArrayList<Node> node2 = shortestPathToNode(node, data2);
         int temp =-1;
         for(int i=node1.size()-1 , j =node2.size()-1;i>=0 && j>=0;i--,j--){
-            if(node1.get(i)!=node2.get(j)){
+            if(node1.get(i).data!=node2.get(j).data){
                 return temp;
             }
-            temp=node1.get(i);
+            temp=node1.get(i).data;
         }
    return temp;
   }
@@ -171,6 +174,40 @@ public class BTree{
             rnode = path.get(i);
             }
     }
+    public static int kfar02(Node node , int k , int data){
+        if(node== null){
+            return -1;
+        }
+        if(node.data == data){
+            kaway(node, k, null);
+            return 1;
+        }
+        int lh = kfar02(node.left, k, data);
+        if(lh!=-1){
+            //mtlb isse kahi node mila 
+            if(lh==k){
+                System.out.println(node.data);
+                // return lh+1;
+            }
+            else{
+                kaway(node, k-(lh), node.left);
+            }
+            // yahi se return kardo 
+            return lh+1;
+        }
+        int rh = kfar02(node.right, k, data);
+        if(rh!=-1){
+            if(rh==k){
+                System.out.println(node.data);
+                // return rh+1;
+            }
+            else{
+                kaway(node, k-(rh), node.right);
+            }
+            return rh+1;
+        }
+        return -1;
+    }
        
-
+// ----- one function(find , height , prec , ssucc , ceil , floor , diameter)------------
 }
