cf2578de88e18a65800ee72231021aeba1435d33
anay-jain <anayjain98@gmail.com>
2020-01-27 03:00:15 +0530

    update

--- pep/lecStack/stack01.java
+++ pep/lecStack/stack01.java
@@ -1,6 +1,97 @@
 import java.util.Stack;
 
 public class stack01{
+    // ------ INFIX EVALUATION ---------------------------------------------------------------
+    public static int precedence(char ch ){
+        if(ch == '+' || ch  == '-'){
+            return 1;
+        }
+        else if(ch == '*' || ch == '/'){
+            return 2 ;
+        }
+        else  if(ch == '^'){
+            return 3;
+        }
+        else{
+            return -1;
+        }
+}
+public static int calculate(char ch , int val1 , int val2){
+    if(ch == '+'){
+        return val1+val2;
+
+    }
+    else if(ch == '-'){
+        return val2-val1;
+    }
+    else if(ch == '*'){
+        return val2*val1;
+
+    }
+    else if(ch == '/'){
+        return val2/val1;
+    }
+    else if(ch == '^'){
+     return (int) Math.pow(val2, val1);
+    }
+    else{
+        return -1;
+    }
+}
+    public static int infixeval(String str){
+        Stack<Integer> num = new Stack<>();
+        Stack<Character> operatorStack = new Stack<>();
+        
+        for(int i=0;i<str.length();i++){
+            char ch = str.charAt(i);
+            //  cant be    if (ch >= '0' && ch <= '9')
+            if(ch-'0' >=0 && ch-'0'<=9){ // this means it is a single dight num
+                // System.out.print(ch+" ");
+                num.push(ch-'0');
+            }else if(ch=='('){
+                operatorStack.push(ch);
+            }else if(ch == ')'){
+                while(operatorStack.size()>0 && operatorStack.peek()!='('){
+                    int val1= num.pop();
+                        int val2 = num.pop();
+                        char ch1 =  operatorStack.pop();
+                        int ans = calculate(ch1, val1, val2);
+                        num.push(ans);
+                }
+                if(operatorStack.size()>0){
+                    operatorStack.pop();
+                }else{
+                    return -1;
+                }
+                    
+
+            }
+            else{
+                if(operatorStack.size()==0){
+                    operatorStack.push(ch);
+                }
+                else {
+                    // resolve condition
+                    while(operatorStack.size()>0 &&operatorStack.peek()!='(' && precedence(ch)<=precedence(operatorStack.peek())){
+                        int val1 = num.pop();
+                        int val2 = num.pop();
+                        int ans = calculate(operatorStack.pop(), val1, val2);
+                        num.push(ans);
+                    }
+                    operatorStack.push(ch);
+            }
+        }
+        }
+        while(operatorStack.size()>0){
+            int val1 = num.pop();
+            int val2 = num.pop();
+            int ans = calculate(operatorStack.pop(), val1, val2);
+            num.push(ans);
+        }
+        return num.peek();
+    }
+
+    // -- Just previous greater element -------------------------------------------------------------
     public static  void  justPrevGreaterElement(int[] arr){
         // creating a new stack
         Stack <Integer> st = new Stack<>();
@@ -20,6 +111,12 @@ public class stack01{
     }
 
     }
+    // INFIX TO POSTFIX EVALUATION-----------------------------------------------------------
+    
+
+
+
+    // Largest area in rectangle------------------------------------------------------------
     public static int largestareainrectangle(int[] arr){
         Stack<Integer> st = new Stack<>();
         
@@ -268,11 +365,13 @@ static  String str = "[{((a+b))+(c+d)}]";
         // int[] arr = {2,1,5,6,2,3};
         // System.out.println(largestareainrectangle(arr));
         // System.out.println(largestvalidParenthesis("()(()"));
-        removeUnnecessaryBrackets();
-        for(int i=0;i<str.length();i++){
-            if(str.charAt(i)!='$') System.out.print(str.charAt(i));
-        }
+        // removeUnnecessaryBrackets();
+        // for(int i=0;i<str.length();i++){
+        //     if(str.charAt(i)!='$') System.out.print(str.charAt(i));
+        // }
         // System.out.println(str);
-        
+            String s = "8+4*3-9/3^(2-1)";
+            System.out.println(infixeval(s)); 
     }
+
 }
