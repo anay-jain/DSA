6f99e1932c0ab290f978417ec974d32e968831bc
anay-jain <anayjain98@gmail.com>
2020-07-30 17:48:05 +0530

    leetcode

--- DS Topics/Trees/tree02.java
+++ DS Topics/Trees/tree02.java
@@ -1,17 +1,14 @@
 public class tree02 {
     static int max = -1e6;
     public int maxPathSum(TreeNode root) {
-        if(root == null ) return 0;
-        if(root.left == null && root.right == null){
-            return root.val;
-        }
-        int left  = maxPathSum(root.left);
-        int right = maxPathSum(root.right);
-        // int temp1 = Math.max(right , left);
-        int temp2 = Math.max(left+root.data,right+root.data);
-        max = Math.max(max, temp2);
-        max = Math.max(max , left+right+root.data);
-        max = Math.max(max , root.data);
+        int left = maxPathSum_(root.left);
+        int right = maxPathSum_(root.right);
+        int temp = Math.max(left , right);
+        int temp1 =  Math.max(temp+root.val , root.val);
+        int temp2 = Math.max(root.val+left+right,temp1);
+        max = Math.max(temp2 ,max );
         return temp1;
+
     }
+    public int k
 }
