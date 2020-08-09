9923a57bf8eabc59e6d7cb728c25bf216a5d38ac
anay-jain <anayjain98@gmail.com>
2020-08-09 15:56:53 +0530

    update

--- DS Topics/Trees/tree02.java
+++ DS Topics/Trees/tree02.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
@@ -61,4 +64,53 @@ public class tree02 {
          findparent(node.left, par);
          findparent(node.right, par);
      }
+
+     // lc 1038 
+     static int sum =0;
+     public TreeNode bstToGst(TreeNode root) {
+        if(root == null) return null;
+        TreeNode(root.right);
+        root.val += sum;
+        TreeNode(root.left);
+    }
+    // lc 199
+    public List<Integer> rightSideView(TreeNode root) {
+        if(root == null) return new ArrayList<>();
+        Queue<TreeNode> que =  new LinkedList<>();
+        que.add(root);
+        List<Integer> li = new ArrayList<>();
+        while(que.size()>0){
+            int size = que.size();
+            while(size==0){
+                TreeNode rnode = que.remove();
+                if(rnode.left!=null) que.add(rnode.left);
+                if(rnode.right!=null) que.add(rnode.right);
+                if(size==1) li.add(que.peek().val);
+                size--;
+            }
+        }
+        return li;
+    }
+    public void bs
+    
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        if(root == null) return new ArrayList<>();
+        Queue<TreeNode> que = new LinkedList<>();
+        List<List<Integer>> li = new ArrayList<>();
+
+        que.add(root);
+        while(que.size()>0){
+            int size= que.size();
+            List<Integer> arr = new ArrayList<>();
+            while(size-->0){
+                TreeNode rnode = que.remove();
+                arr.add(rnode.val);
+                if(rnode.left!=null) que.add(rnode.left);
+                if(rnode.right!=null) que.add(rnode.right);
+            }
+            Collections.sort(arr);
+            li.add(arr);
+        }
+        return li;
+    }
 }
